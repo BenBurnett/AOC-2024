0d3759f51265522a3b4236d3094134dfce19ced7
Ben Burnett <burnett.73@gmail.com>
2024-12-13 17:16:44 -0500

    Add day 10

---
+++ 10/part2.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+
+class Trail:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def score(self):
+        seen = []
+        queue = deque()
+        queue.append((
+            0,
+            self.x,
+            self.y,
+        ))
+
+        while queue:
+            val, x, y = queue.popleft()
+
+            if x < 0 or y < 0 or x >= xmax or y >= ymax:
+                continue
+
+            if int(lines[y][x]) != val:
+                continue
+
+            if val == 9:
+                seen.append((x, y))
+                continue
+
+            queue.append((val + 1, x + 1, y))
+            queue.append((val + 1, x - 1, y))
+            queue.append((val + 1, x, y + 1))
+            queue.append((val + 1, x, y - 1))
+
+        return len(seen)
+
+
+with open('input.txt') as f:
+    lines = [line.strip() for line in f]
+
+xmax = len(lines[0])
+ymax = len(lines)
+
+score = 0
+for yi in range(ymax):
+    for xi in range(xmax):
+        if lines[yi][xi] == '0':
+            score += Trail(xi, yi).score()
+
+print(score)
